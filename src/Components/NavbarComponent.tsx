e0f17239044c48604878331c8b541eb2208abb0f
MundoDeSanti <santiagomm44@gmail.com>
2025-05-03 12:38:51 -0700

    Page and navebar changes

--- src/Components/NavbarComponent.tsx
+++ src/Components/NavbarComponent.tsx
@@ -1,15 +1,19 @@
-import { Navbar, NavbarBrand} from "flowbite-react";
+import { Button, Navbar, NavbarBrand } from "flowbite-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export function NavbarComponent() {
   return (
     <div className="sticky top-0 z-40">
-    <Navbar fluid className="border-solid border-b-4 border-[#2EC4B6] dark:border-[#2EC4B6] bg-white dark:bg-white">
-    <NavbarBrand as={Link} href="#">
-      <Image src="./assets/Kamino.svg" height={70} width={70} className="mr-3 h-20" alt="Kamino"/>
-    </NavbarBrand>
-    </Navbar>
+      <Navbar fluid className="border-solid border-b-4 border-[#2EC4B6] dark:border-[#2EC4B6] bg-white dark:bg-white">
+        <NavbarBrand as={Link} href="#">
+          <Image src="./assets/Kamino.svg" height={70} width={70} className="mr-3 h-20" alt="Kamino" />
+        </NavbarBrand>
+        <div className="flex gap-x-6">
+          <Button className="bg-[#FF9F1C] hover:bg-[#FFBF69]">Login</Button>
+          <Button className="bg-[#FF9F1C] hover:bg-[#FFBF69]">Sign Up</Button>
+        </div>
+      </Navbar>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <Link href={"/Home"}>
-        <Button className="bg-[#FF9F1C] hover:bg-[#FFBF69]">Login</Button>
-        <Button className="bg-[#FF9F1C] hover:bg-[#FFBF69]">Sign Up</Button>
-      </Link>
+      
     </div>
   );
 }
