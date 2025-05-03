cb36856942921a9918bf4d9091fc8930252ec1a8
Kass-S <kassiespacek101@gmail.com>
2025-05-03 13:14:56 -0700

    small changes

--- src/app/(pages)/Login/page.tsx
+++ src/app/(pages)/Login/page.tsx
@@ -7,21 +7,21 @@ const Login = () => {
     <div className="m-2 p-5 bg-white md:w-[400px] rounded-lg">
     <div>
         <div className="mb-2 block">
-          <Label htmlFor="email1">Username or Password</Label>
+          <Label htmlFor="email1" className='dark:text-black'>Username or Password</Label>
         </div>
-        <TextInput id="email1" type="email" required />
+        <TextInput id="email1" type="email" required className='bg-white dark:bg-white' />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password1">Password</Label>
+          <Label htmlFor="password1" className='dark:text-black'>Password</Label>
         </div>
-        <TextInput id="password1" type="password" required />
+        <TextInput id="password1" type="password" className='bg-white dark:bg-white' required />
       </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="remember" />
-        <Label htmlFor="remember">Remember me</Label>
+      <div className="flex items-center gap-2 mt-3">
+        <Checkbox id="remember" className='bg-gray-50 dark:bg-gray-50' />
+        <Label htmlFor="remember" className='dark:text-black'>Remember me</Label>
       </div>
-      <Button type="submit">Submit</Button>
+      <Button type="submit" className='mt-5'>Submit</Button>
     </div>
 </div>
   )
--- src/app/(pages)/SignUp/page.tsx
+++ src/app/(pages)/SignUp/page.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import { Button, Label, TextInput } from 'flowbite-react'
+ import Link from 'next/link'
+ import React from 'react'
+ 
+ const SignUp = () => {
+   return (
+     <div className='flex justify-center items-center min-h-screen'>
+     <div className="m-2 p-5 bg-white md:w-[400px] rounded-lg">
+     <div>
+         <div className="mb-2 block">
+           <Label htmlFor="name" className='dark:text-black'>Name</Label>
+         </div>
+         <TextInput id="name" type="name" required className='bg-white dark:bg-white' />
+       </div>
+     <div>
+         <div className="mb-2 block">
+           <Label htmlFor="email1" className='dark:text-black'>Email</Label>
+         </div>
+         <TextInput id="email1" type="email" required className='bg-white dark:bg-white' />
+       </div>
+       <div>
+         <div className="mb-2 block">
+           <Label htmlFor="password2" className='dark:text-black'>Confirm Password</Label>
+         </div>
+         <TextInput id="password2" type="password" required className='bg-white dark:bg-white' />
+       </div>
 
-const SignUp = () => {
-  return (
-    <div className='flex justify-center items-center min-h-screen'>
-        <div className="m-2 p-5 bg-white md:w-[400px] rounded-lg">
+       <div>
+         <div className="mb-2 block">
+           <Label htmlFor="password1" className='dark:text-black'>Password</Label>
+         </div>
+         <TextInput id="password1" type="password" required className='bg-white dark:bg-white' />
+       </div>
 
-        </div>
-    </div>
-  )
-}
+       <Link href={"/Home"} >
+       <Button type="submit" className=' bg-[#FF9F1C] hover:bg-[#FFBF69] pointer-cursor my-2 mt-5'>Sign Up</Button>
+       </Link>
 
-export default SignUp
+       <div className="text-center flex justify-center gap-2 mt-5">
+         <p>Create User</p>
+         <p> | </p>
+         <Link href="/Login">
+         <p className='underline text-blue-500'>Login</p>
+         </Link>
+       </div>
+     </div>
+ 
+ </div>
+   )
+ }
+ 
+ export default SignUp
