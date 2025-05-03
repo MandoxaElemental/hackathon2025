d2e0d7ba40fd67b1ebcb786203f8160f1e69822a
Gianpaolo Reinares <gprcards@gmail.com>
2025-05-03 12:06:04 -0700

    Saving

--- src/Components/NavbarComponent.tsx
+++ src/Components/NavbarComponent.tsx
@@ -7,7 +7,7 @@ export function NavbarComponent() {
     <div className="sticky top-0 z-40">
     <Navbar fluid rounded className="border-solid border-b-4 border-[#2EC4B6]">
     <NavbarBrand as={Link} href="#">
-      <Image src="/assets/kamino.svg" height={70} width={70} className="mr-3 h-20" alt="Kamino"/>
+      <Image src="./assets/kamino.svg" height={70} width={70} className="mr-3 h-20" alt="Kamino"/>
     </NavbarBrand>
     </Navbar>
     </div>
