a7002df0ca3cb52796f0fc01d8c68f039d260af5
Gianpaolo Reinares <gprcards@gmail.com>
2025-05-03 11:53:38 -0700

    Saving

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
       <Link href={"/Home"}>
-        <Button>Click Me</Button>
+        <Button className="bg-[#FF9F1C] hover:bg-[#FFBF69]">Login</Button>
+        <Button className="bg-[#FF9F1C] hover:bg-[#FFBF69]">Sign Up</Button>
       </Link>
     </div>
   );
