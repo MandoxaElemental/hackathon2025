d16b843818f6ad51665502770bed49dfda35dba2
Kass-S <kassiespacek101@gmail.com>
2025-05-03 12:56:34 -0700

    deleted unneeded pages

---
+++ src/app/(pages)/SignUp/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+        <div className="m-2 p-5 bg-white md:w-[400px] rounded-lg">
+
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
